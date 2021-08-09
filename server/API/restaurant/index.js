a7914d80924f8e8f2159ddd8b9997fa07da1da1f
vaishud12 <vaishuu2397@gmail.com>
2021-08-09 19:31:59 +0530

    restaurant Api

---
+++ server/API/restaurant/index.js
@@ -0,0 +1,76 @@
+// Libraries
+import express from "express";
+import passport from "passport";
+
+// Database modal
+import { RestaurantModel } from "../../database/allModels"
+
+const Router = express.Router();
+/*
+Route   /
+Des     Get all the restaurant details based in city
+Params  none
+Access  Public
+Method  GET
+*/
+Router.get("/", (req, res) => {
+    try {
+        const { city } = req.query;
+        const  restaurants = await RestaurantModel.find({ city });
+
+        return res.json({ Restaurants });
+    }   catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+});
+
+/*
+Route   /
+Des     Get individual restaurant details based on id
+Params  id
+Access  Public
+Method  GET
+*/
+Router.get("/:id", async (req, res) => {
+    try  {
+        const { _id } = req.params;
+        const restaurant = await RestaurantModel.findOne(_id);
+        if (!restaurant)
+          return res.status(404).json({ error: "Restaurant Not Found" });
+        
+        return res.json({ restaurant });
+    }   catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+Route   /search
+Des     Get individual restaurant details based on search string
+Params  none
+Body    searchString
+Access  Public
+Method  GET
+*/
+Router.get("/search",async (req, res) => {
+    try {
+        const { searchString } = req.body;
+        const restaurants = await RestaurantModel.find({
+            name: { $regex: searchString, $options: "i" },
+        });
+
+        if (!restaurants)
+          return res
+            .status(404)
+            .json({ error: `No Restaurant matched with ${searchString}` });
+
+        return res.json({ restaurants });
+    }   catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
+
+// /*A-Z*/($regex-match this pattern tell to compiler)
--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import googleAuthConfig from "./config/google.config";
 
 // microservice routes
 import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+
 
 // Database connection 
 import ConnectDB from "./database/connection";
@@ -31,6 +33,7 @@ googleAuthConfig(passport);
 
 // Application routes
 zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
 
 zomato.get("/" , (req, res) => res.json({ message: "setup success" }));
 
