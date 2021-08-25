4d5b53b08482d1c517d2e32b91c92617660dfd3d
vaishud12 <vaishuu2397@gmail.com>
2021-08-25 22:14:59 +0530

    Dining & Nightlfe

--- client/src/components/master.jsx
+++ client/src/components/master.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 
 // components
 import Delivery from "./Delivery";
-
+import Dining from "./Dining";
+import NightLife from "./NightLife";
 
 const Master = () => {
     const { type } = useParams();
@@ -11,6 +12,8 @@ const Master = () => {
     return (
         <div className="my-5">
             {type === "delivery" && <Delivery />}
+            {type === "dining" && <Dining />}
+            {type === "night" && <NightLife />}
         </div>
     );
 };
